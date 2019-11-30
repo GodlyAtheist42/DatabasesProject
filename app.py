9424ff11acf2a468dfcd094b4cdc312d44a05120
NamanChimnani <nc1998@nyu.edu>
2019-11-30 14:41:23 -0500

    - Added search method and search results html file - searching gives error code 400

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 conn = pymysql.connect(host = '127.0.0.1',
                        user = 'root',
-                       password = '',
+                       port = 8889,
+                       password = 'root',
                        db = 'finsta',
                        charset = 'utf8mb4',
                        )
@@ -251,6 +252,27 @@ def tagRequest():
         cursor.execute(query, (user, pid))
         conn.commit()
     return redirect(url_for('home'))
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    user = session['username']
+    cursor = conn.cursor()
+    pid = request.form['searchUser']
+    query = 'SELECT filepath, caption, photoPoster, photoID \
+             FROM photo \
+             WHERE (photoPoster = %s) AND ((allFollowers = 1 AND photoPoster in (SELECT username_followed FROM follow WHERE username_follower = %s AND followStatus = 1)) \
+             OR \
+             (      photoID in (SELECT PhotoId \
+				    FROM SharedWith \
+					WHERE (groupName,groupOwner ) IN (SELECT groupName,owner_username \
+                                                          FROM BelongTo \
+					                                      WHERE member_username = %s)\
+			)))'
+    cursor.execute(query, (pid, user, user))
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('searchresults.html', photoData=data)
+
 
 @app.route('/logout')
 def logout():
